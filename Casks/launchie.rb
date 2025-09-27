ca049bba57df9ce57ad144f0b04b2f34e70cc4e3
Nick <nfriedrich1990@gmail.com>
2025-09-27 08:27:44 +0200

    Create launchie.rb

---
+++ Casks/launchie.rb
@@ -0,0 +1,12 @@
+cask "launchie" do
+  version "1.0.8"
+  sha256 "b966925d084b8e23cdf63f6a4ee974e04a74d579a2ec09b4c844ce36ab2e50d6"
+
+  url "https://github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/releases/download/#{version}/Launchie-#{version}.dmg",
+      verified: "github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/"
+  name "Launchie"
+  desc "Launchpad replacement for macOS"
+  homepage "https://www.launchie.app"
+
+  app "Launchie.app"
+end
