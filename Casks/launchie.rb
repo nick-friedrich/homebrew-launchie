b6240664c6c4420abfefa43abbafc0ee7c1411f7
Nick <nfriedrich1990@gmail.com>
2025-09-28 07:29:32 +0200

    Update launchie.rb

--- Casks/launchie.rb
+++ Casks/launchie.rb
@@ -1,6 +1,6 @@
 cask "launchie" do
-  version "1.0.8"
-  sha256 "b966925d084b8e23cdf63f6a4ee974e04a74d579a2ec09b4c844ce36ab2e50d6"
+  version "1.0.9"
+  sha256 "c716b9727a4cd7fea24d092230f9d701788be54da614f8999f1b5ba3aa32c884"
 
   url "https://github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/releases/download/#{version}/Launchie_#{version}.dmg",
       verified: "github.com/nick-friedrich/launchie-launchpad-replacement-mac-os/"
